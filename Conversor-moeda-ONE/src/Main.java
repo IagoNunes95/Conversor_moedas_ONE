a3c96a7bbea4f85530270bb3de7857c996adc19c
IagoNunes95 <iagonunesfrancino@hotmail.com>
2025-05-27 01:00:54 -0300

    terceiro commit teste

--- Conversor-moeda-ONE/src/Main.java
+++ Conversor-moeda-ONE/src/Main.java
@@ -3,5 +3,6 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("iniciando projeto e subindo github");
 		System.out.println("testando segundo commit");
+		System.out.println("testando segundo commit");
 	}
 }
