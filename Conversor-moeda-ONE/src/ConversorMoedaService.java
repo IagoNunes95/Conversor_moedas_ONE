deef575ab19a3b78914eb23f8f2bf28de49643e8
IagoNunes95 <iagonunesfrancino@hotmail.com>
2025-05-28 17:03:19 -0300

    Criação do menu - finalização do projeto.

--- Conversor-moeda-ONE/src/ConversorMoedaService.java
+++ Conversor-moeda-ONE/src/ConversorMoedaService.java
@@ -12,23 +12,32 @@ public class ConversorMoedaService {
     private final String apiKey = "1147780d3ccfa0491b2c8861";
     private final String baseUrl = "https://v6.exchangerate-api.com/v6/";
     private final Gson gson;
-		
+	
+    //método padrão para inicializar o httpclient e o gson na instanciação da classe.
     public ConversorMoedaService() {
         this.httpClient = HttpClient.newHttpClient();
         this.gson = new Gson();
     }
     
+    
+    //método para acessar a API da exchangerate e retornar o resultado da conversão.
     public double converter(String moedaOrigem, String moedaDestino, double valor) throws Exception {
         String url = baseUrl + apiKey + "/pair/" + moedaOrigem + "/" + moedaDestino + "/" + valor;
+        
+        //criando a "carta" para requisição.
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(url))
                 .GET()
                 .build();
         
+        //realizando a requisição
         HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
         
+        //Transformando o retorno(Json) em um objeto java do tipo json.
         JsonObject json = gson.fromJson(response.body(), JsonObject.class);
         
+        
+        //realizando o tratamento do resultado caso dê erro.
         if (json.get("result").getAsString().equals("success")) {
             return json.get("conversion_result").getAsDouble();
         } else {
--- Conversor-moeda-ONE/src/Main.java
+++ Conversor-moeda-ONE/src/Main.java
@@ -1,10 +1,50 @@
+import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("iniciando projeto e subindo github");
-		System.out.println("testando segundo commit");
-		System.out.println("testando segundo commit");
-		System.out.println("testando finalmente isso");
 		
+		Scanner scanner = new Scanner(System.in);
+		
+		ConversorMoedaService conversor = new ConversorMoedaService();
+		
+		//criando array das moedas para o nosso menu.
+		String[] moedas = { "BRL", "USD", "EUR", "GBP", "JPY", "ARS" };
+
+		System.out.println("========== CONVERSOR DE MOEDAS ==========\n");
+
+		//laço para leitura do array para um menu mais simples.
+		System.out.println("Escolha a moeda de ORIGEM:");
+		for (int i = 0; i < moedas.length; i++) {
+			System.out.printf("%d - %s\n", i + 1, moedas[i]);
+		}
+		System.out.print("Digite o número da moeda de origem: ");
+		int opcaoOrigem = scanner.nextInt();
+		String moedaOrigem = moedas[opcaoOrigem - 1];
+
+		
+		System.out.println("\nEscolha a moeda de DESTINO:");
+		for (int i = 0; i < moedas.length; i++) {
+			System.out.printf("%d - %s\n", i + 1, moedas[i]);
+		}
+		System.out.print("Digite o número da moeda de destino: ");
+		int opcaoDestino = scanner.nextInt();
+		String moedaDestino = moedas[opcaoDestino - 1];
+
+		
+		System.out.print("\nDigite o valor que deseja converter: ");
+		double valor = scanner.nextDouble();
+
+		System.out.println("\nConvertendo...");
+		
+		//iniciando nossa API para retornar a conversão das moedas.
+		try {
+			double resultado = conversor.converter(moedaOrigem, moedaDestino, valor);
+			System.out.printf("\n%.2f %s equivalem a %.2f %s\n", valor, moedaOrigem, resultado, moedaDestino);
+		} catch (Exception e) {
+			System.out.println("Erro ao converter moedas: " + e.getMessage());
+		}
+
+		scanner.close();
 	}
+
 }
